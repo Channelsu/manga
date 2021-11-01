e335909d9dbf416d1262a503750f331cd345d7bc
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-11-01 20:56:22 +0900

    カード作成

---
+++ pages/mangas/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="mangas">
+    <article class="manga">
+      <div></div>
+      <h1>マンガ名</h1>
+      <p>説明文</p>
+    </article>
+    <article class="manga">
+      <div></div>
+      <h1>マンガ名</h1>
+      <p>説明文</p>
+    </article>
+  </section>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+.mangas {
+  display: flex;
+  flex-flow: row wrap;
+}
+
+.manga {
+  box-sizing: border-box;
+  width: 280px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #aaa;
+}
+
+</style>
