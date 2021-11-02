e4c238681fd15403576d7eac0efb1fec5b059e95
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-11-02 22:11:07 +0900

    サムネイル追加

--- pages/mangas/index.vue
+++ pages/mangas/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="mangas">
     <article class="manga">
-      <div></div>
+      <div class="thumbnail" style="background-image: url(https://cdn-scissors.gigaviewer.com/image/scale/b6e24b6a28f7e90515663c0055534678c962168e/enlarge=0;height=484;no_unsharpmask=1;quality=90;version=1;width=484/https%3A%2F%2Fcdn-ak-img.shonenjumpplus.com%2Fpublic%2Fseries-thumbnail%2F10833519556325021853-84c0dbccc6b51dbff40781c7a6621ae1%3F1635721960)"></div>
       <h1>マンガ名</h1>
       <p>説明文</p>
     </article>
@@ -31,6 +31,13 @@
   padding: 8px;
   border: 1px solid #ccc;
   box-shadow: 0 2px 2px #aaa;
+}
+
+.thumbnail {
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 200px;
 }
 
 </style>
