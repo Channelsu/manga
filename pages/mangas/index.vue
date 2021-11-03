8334331b890e0af1f93ac33dc501081aa51c3c27
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-11-03 10:26:23 +0900

    カードのレスポンシブ対応

--- pages/mangas/index.vue
+++ pages/mangas/index.vue
@@ -6,7 +6,7 @@
       <p>説明文</p>
     </article>
     <article class="manga">
-      <div></div>
+      <div class="thumbnail" style="background-image: url(https://cdn-scissors.gigaviewer.com/image/scale/49885d613e7e6da94fa2d34d158289fbbf3a8408/enlarge=0;height=484;no_unsharpmask=1;quality=90;version=1;width=484/https%3A%2F%2Fcdn-ak-img.shonenjumpplus.com%2Fpublic%2Fseries-thumbnail%2F10834108156670462834-c4d593a1af4ddfad2a95fdfd93ccfa1b%3F1634944365)"></div>
       <h1>マンガ名</h1>
       <p>説明文</p>
     </article>
@@ -23,6 +23,8 @@
 .mangas {
   display: flex;
   flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
 }
 
 .manga {
@@ -31,6 +33,7 @@
   padding: 8px;
   border: 1px solid #ccc;
   box-shadow: 0 2px 2px #aaa;
+  margin: 10px;
 }
 
 .thumbnail {
