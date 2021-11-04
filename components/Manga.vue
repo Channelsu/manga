f5ce336c7d03bbc69982101020f747b0341ea202
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-11-04 21:44:28 +0900

    マンガカード作成

---
+++ components/Manga.vue
@@ -0,0 +1,17 @@
+<template>
+  <article class="manga">
+    <div class="thumbnail" :style="{ backgroundImage: 'url(' + tumbnail + ')' }"></div>
+    <h1>{{ title }}</h1>
+    <p>{{ previewText }}</p>
+  </article>
+</template>
+
+<script>
+  export default {
+    props: ['thumbnail', 'title', 'previewText']
+  }
+</script>
+
+<style scoped>
+
+</style>
