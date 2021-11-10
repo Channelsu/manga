47487061e67de94bafecaa01909cfe51caf8d6c7
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-11-10 22:20:24 +0900

    マンガ詳細ページの枠組み作成

--- pages/mangas/_id/index.vue
+++ pages/mangas/_id/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <p>{{ $route.params.id }}</p>
+  <section class="single-manga">
+    <h1>TITLE</h1>
+    <div>
+      <img src="" alt="">
+    </div>
+    <p>マンガの詳細</p>
+  </section>
 </template>
 
 <script>
