561ed4c5a6e6dbe137f8aae652e908ae3045561e
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-11-11 22:46:57 +0900

    マンガ名修正

--- pages/mangas/index.vue
+++ pages/mangas/index.vue
@@ -3,12 +3,12 @@
     <Manga 
       thumbnail="https://cdn-scissors.gigaviewer.com/image/scale/b6e24b6a28f7e90515663c0055534678c962168e/enlarge=0;height=484;no_unsharpmask=1;quality=90;version=1;width=484/https%3A%2F%2Fcdn-ak-img.shonenjumpplus.com%2Fpublic%2Fseries-thumbnail%2F10833519556325021853-84c0dbccc6b51dbff40781c7a6621ae1%3F1635721960"
       title="NARUTO"
-      previewText="忍者まんが"
+      previewText="忍者マンガ"
     />
     <Manga 
-      thumbnail="    https://cdn-scissors.gigaviewer.com/image/scale/49885d613e7e6da94fa2d34d158289fbbf3a8408/enlarge=0;height=484;no_unsharpmask=1;quality=90;version=1;width=484/https%3A%2F%2Fcdn-ak-img.shonenjumpplus.com%2Fpublic%2Fseries-thumbnail%2F10834108156670462834-c4d593a1af4ddfad2a95fdfd93ccfa1b%3F1634944365"
-      title="NARUTO"
-      previewText="忍者まんが"
+      thumbnail="https://cdn-scissors.gigaviewer.com/image/scale/49885d613e7e6da94fa2d34d158289fbbf3a8408/enlarge=0;height=484;no_unsharpmask=1;quality=90;version=1;width=484/https%3A%2F%2Fcdn-ak-img.shonenjumpplus.com%2Fpublic%2Fseries-thumbnail%2F10834108156670462834-c4d593a1af4ddfad2a95fdfd93ccfa1b%3F1634944365"
+      title="ホイッスル"
+      previewText="サッカーマンガ"
     />
   </section>
 </template>
